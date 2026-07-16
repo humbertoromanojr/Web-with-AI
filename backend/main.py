9d0e3e680eba4a37a82afd52540b230660f42dc4
humbertoromanojr <astronomi@gmail.com>
2026-07-16 20:03:23 -0300

    Updating endpoint name

--- backend/main.py
+++ backend/main.py
@@ -11,10 +11,10 @@ def hello_world():
     return {"mensagem": "Olá, mundo! 🌍"}
 
 
-# Define a rota GET para o endpoint "/figurinhas"
-@app.get("/figurinhas")
-def listar_figurinhas():
-    # Retorna uma lista com figurinhas de exemplo
+# Define a rota GET para o endpoint "/bands"
+@app.get("/bands")
+def list_bands():
+    # Retorna uma lista com bands de exemplo
     return [
         {"id": 1, "nome": "Sarcofago", "categoria": "AMÉRICA DO SUL"},
         {"id": 2, "nome": "Slayer", "categoria": "AMÉRICA DO NORTE"},
